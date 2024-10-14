29aea3f64f9f1682c49854d5e964db5b666419e4
shij <jevin.shj@gmail.com>
2024-10-14 10:26:34 +0800

    fix(payment): remove auto set completed_at of payment collection

--- packages/modules/payment/src/services/payment-module.ts
+++ packages/modules/payment/src/services/payment-module.ts
@@ -1077,11 +1077,6 @@ export default class PaymentModuleService
           authorized_amount: authorizedAmount,
           captured_amount: capturedAmount,
           refunded_amount: refundedAmount,
-          completed_at:
-            paymentCollection.completed_at ??
-            (MathBN.gte(netCapturedAmount, paymentCollection.raw_amount)
-              ? new Date()
-              : null),
         },
         sharedContext
       )
